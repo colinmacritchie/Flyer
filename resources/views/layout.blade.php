21a4d2a98919510152aab669ff47e097a8072399
Colin MacRitchie <ciscopowdercowboy@gmail.com>
2015-08-25 13:53:10 -0600

    updated routes, updated home, added layout file

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Flyer</title>
+	<link rel="stylesheet" href="/css/app.css">
+
+</head>
+
+<body>
+
+		<div class="container">
+
+			@yield('content')
+
+		</div>
+
+
+</body>
+
+</html>	
